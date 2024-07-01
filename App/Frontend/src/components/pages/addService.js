371ed58d750667739c5c9475e88cd08fde52ff3a
Thompson Ng <133816126+Woowoonetwork@users.noreply.github.com>
2024-07-01 06:31:05 -0700

    Added basic styling
    Added basic styling using account.css for consistency with account.js form and its own .css file
    Altered styling of buttons on the right in the navbar to size itself to fit its text in a single horizontal line

--- App/Frontend/src/components/pages/addService.js
+++ App/Frontend/src/components/pages/addService.js
@@ -1,5 +1,8 @@
 import {Formik, Form, Field, ErrorMessage} from 'formik';
 import axios from 'axios';
+// using some aspects of account.css for consistency with account.js form styling
+import '../account/account.css';
+import './addService.css';
 
 
 const renderError = (message) => <p className="warning">{message}</p>;
@@ -9,60 +12,64 @@ const AddService = () => {
 
     return(
         <>
-            <Formik
-            initialValues={{
-                service: "",
-                description: ""
-            }}
-            onSubmit={(values, actions) => {
-                actions.setSubmitting(true);
-                // console.log(values);
-                (async () => {
-                    // submit to DB
-                    axios.post('http://localhost:8080/services', values)
-                    .then((response)=>{
-                        if(response.data){
-                            console.log(response.data);
-                        }
-                        actions.setSubmitting(false);
-                    })
-                    .then(() => {
-                        // window.location.assign('/');
-                    });
-                })();
-            }}
-            >
-			{({handleSubmit, values}) => (
-                <Form
-                    className="addServiceForm"
-                    onSubmit={handleSubmit}
-                >
-                    <div className="addServiceContainer">
-                        <p>Service Name:</p>
-                        <Field
-                            type="text"
-                            id="service"
-                            name="service"
-                            label="Service Name"
-                            autoComplete="sName"
-                            required
-                        />
-                        <ErrorMessage name="service" render={renderError} />
-
-                        <p>Service Description:</p>
-                        <Field
-                            name="description"
-                            as="textarea"
-                            id="serviceDescription"
-                            required
-                        />
-                        <ErrorMessage name="description" render={renderError} />
-                        
-                        <button type='submit' className="btn--login">Save</button>
-                    </div>
-                </Form>
-            )}
-            </Formik>
+            <div className="userProfile">
+                <div className="userContainer">
+                    <Formik
+                    initialValues={{
+                        service: "",
+                        description: ""
+                    }}
+                    onSubmit={(values, actions) => {
+                        actions.setSubmitting(true);
+                        // console.log(values);
+                        (async () => {
+                            // submit to DB
+                            axios.post('http://localhost:8080/services', values)
+                            .then((response)=>{
+                                if(response.data){
+                                    console.log(response.data);
+                                }
+                                actions.setSubmitting(false);
+                            })
+                            .then(() => {
+                                // window.location.assign('/');
+                            });
+                        })();
+                    }}
+                    >
+                    {({handleSubmit, values}) => (
+                        <Form
+                            className="accountInfoEditorForm"
+                            onSubmit={handleSubmit}
+                        >
+                            <div className="accountInfoEditorContainerContainer">
+                                <p>Service Name:</p>
+                                <Field
+                                    type="text"
+                                    id="service"
+                                    name="service"
+                                    label="Service Name"
+                                    autoComplete="sName"
+                                    required
+                                />
+                                <ErrorMessage name="service" render={renderError} />
+                                <div className="nonTextSignupForm">
+                                    <p>Service Description:</p>
+                                    <Field
+                                        name="description"
+                                        as="textarea"
+                                        id="serviceDescription"
+                                        required
+                                    />
+                                    <ErrorMessage name="description" render={renderError} />
+                                </div>
+                                <button type='submit' className="btn--login">Save</button>
+                            </div>
+                        </Form>
+                    )}
+                    </Formik>
+                </div>
+            </div>
         </>
     )
 }
