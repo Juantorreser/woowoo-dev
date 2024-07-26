393faf4bfcc235f5e5ae3d32ababb20d4e9af2e8
Thompson Ng <133816126+Woowoonetwork@users.noreply.github.com>
2024-07-26 05:35:41 -0700

    Fixed spelling inconsistency
    Corrected manageService to manageServices

--- App/Frontend/src/components/Main/Main.js
+++ App/Frontend/src/components/Main/Main.js
@@ -53,7 +53,7 @@ import SignOut from '../account/signout';
 import Schedule from '../pages/schedule';
 import MessageBox from '../pages/message';
 import AddService from '../pages/addService';
-import ManageService from '../pages/manageServices';
+import ManageServices from '../pages/manageServices';
 
 
 // Main acts as a container for the routing
@@ -75,7 +75,7 @@ const Main = () => {
                     <Route exact path='/signout' element={<SignOut />} />
                     <Route exact path='/message' element={<MessageBox />} />
                     <Route exact path='/addService' element={<AddService />} />
-                    <Route exact path='/manageService' element={<ManageService />} />
+                    <Route exact path='/manageServices' element={<ManageServices />}/>
                 </Routes>
             </div>
 
