43bcd5f6088189f64a8c155cdaa03aee552e0396
Juantorreser <jdtorreserazo@gmail.com>
2025-06-17 18:36:21 -0700

    Footer UI styles Updated

--- App/Frontend/src/components/footer/Footer.js
+++ App/Frontend/src/components/footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
     return(
 		<footer>
 			<div className="footer">
+				<h1>Woo Woo Network 2025</h1>
 				<Link className="footerLink" to="/privacy">Privacy Statement</Link>
 			</div>
 		</footer>
