b45eb49aee0b20d6533aa16a4e07eec9ef15ee39
Juantorreser <jdtorreserazo@gmail.com>
2025-06-30 14:27:37 -0700

    New Home Page Setup

--- App/Frontend/src/components/pages/home.js
+++ App/Frontend/src/components/pages/home.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import AboutPage from './about';
+import FeaturedHealers from '../FeaturedHealers/FeaturedHealers';
 
 function home() {
-    return <Hero />;
+    return <>
+        <Hero />
+        <AboutPage />
+        <FeaturedHealers />
+    </>;
 }
 
 export default home;
